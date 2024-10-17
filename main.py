d3cef7844c3ad8725664a78e3ea385fedc15e897
Huang Po-Wei <71061276+georgepwhuang@users.noreply.github.com>
2024-10-17 14:59:09 +0100

    Minor tweaks

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 from rare_event import iMPS, RecurrentQuantumCircuit, RealDiagonalBlockEncoding, QET
 import numpy as np
 from scipy.special import erf
@@ -7,6 +8,14 @@ from pyqsp.poly import PolyTaylorSeries
 
 import warnings
 warnings.filterwarnings("ignore")
+
+
+LAYERS = 2
+
+DELTA = 20
+THRESHOLD = 0.1
+
+SIMULATE = False
 
 def pcoin(p):
     T0 = np.array([
@@ -25,18 +34,12 @@ mem_0 = imps_pcoin.lft_cform().en_past([0])
 mem_1 = imps_pcoin.lft_cform().en_past([1])
 transition = imps_pcoin.to_unitary()
 
-layers = 2
-base_qubits = layers + 1
+base_qubits = LAYERS + 1
 
-delta = 50
-threshold = 0.1
-
-simu = False
-
-func = lambda x: (erf(delta*(x + threshold)) - erf(delta*(x - threshold))) / 2
+func = lambda x: (erf(DELTA*(x + THRESHOLD)) - erf(DELTA*(x - THRESHOLD))) / 2
 polydeg = 4
 max_scale = 0.9 # Maximum norm (<1) for rescaling.
-true_func = lambda x: np.where(np.abs(x) < threshold, 1, 0)
+true_func = lambda x: np.where(np.abs(x) < THRESHOLD, 1, 0)
 
 
 poly = PolyTaylorSeries().taylor_series(
@@ -72,26 +75,24 @@ def convert_angles(angles):
 phiset = convert_angles(phiset)
 
 dev = qml.device("lightning.qubit", wires=base_qubits)
-if simu:
-    dev2 = qml.device("lightning.qubit", wires=base_qubits+1)
-else: 
-    dev2 = qml.device("lightning.qubit", wires=2*base_qubits+3)
 
 @qml.qnode(dev)
 def circuit():
     RecurrentQuantumCircuit(wires=list(range(base_qubits)),memory_state_prep=mem_0, transition=transition)
     return qml.state()
 
-if simu:
+if SIMULATE:
+    dev2 = qml.device("lightning.qubit", wires=base_qubits+1)
     wires = list(range(1, base_qubits+1))
     ancilla_wires = [0]
 else: 
+    dev2 = qml.device("lightning.qubit", wires=2*base_qubits+3)
     wires = list(range(base_qubits+3, 2*base_qubits+3))
     ancilla_wires = list(range(base_qubits+3))
     
 @qml.qnode(dev2)
 def circuitGate():
-    QET(RealDiagonalBlockEncoding,U=RecurrentQuantumCircuit, wires=wires, ancilla_wires=ancilla_wires, angles=phiset, simulate=simu, memory_state_prep=mem_0, transition=transition)
+    QET(RealDiagonalBlockEncoding,U=RecurrentQuantumCircuit, wires=wires, ancilla_wires=ancilla_wires, angles=phiset, simulate=SIMULATE, memory_state_prep=mem_0, transition=transition)
     return qml.state()
 
 x = circuit().real
@@ -104,4 +105,4 @@ try:
     fig, ax = qml.draw_mpl(circuitGate, decimals=2)()
     fig.savefig('algo.png')
 except:
-    pass
+    print("Figure too long", file=sys.stderr)
